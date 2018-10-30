bf514838cc0ad94b3646a50a7948a25ea95ed64d
yangxh <yangxh@sysware.com.cn>
2018-10-30 14:06:27 +0800

    测试

---
+++ web/src/main/java/com/test/web/models/Test.java
@@ -0,0 +1,16 @@
+package com.test.web.models;
+
+/**
+ * @author : yangxh
+ * @version : 1.0
+ * @create 2018-10-30 14:04
+ * @Team : 系统集成部
+ * @description :
+ **/
+public class Test {
+
+    private String id;
+
+
+
+}
