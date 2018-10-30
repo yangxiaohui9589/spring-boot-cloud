537ccc7c514f486359344cff83486e46a9fecc20
yangxh <yangxh@sysware.com.cn>
2018-10-30 14:07:19 +0800

    测试

---
+++ web/src/main/java/com/test/web/service/TestClient.java
@@ -0,0 +1,13 @@
+package com.test.web.service;
+
+/**
+ * @author : yangxh
+ * @version : 1.0
+ * @create 2018-10-30 14:07
+ * @Team : 系统集成部
+ * @description :
+ **/
+public class TestClient {
+
+
+}
